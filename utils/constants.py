892a8a7bd97464f6e5656a9297ef7a569503855f
saleemhamo <saleemhamo@gmail.com>
2024-07-24 23:23:39 +0300

    updated models path

--- utils/constants.py
+++ utils/constants.py
@@ -22,6 +22,6 @@ CHARADES_ANNOTATIONS_TRAIN = os.path.join(CHARADES_ANNOTATIONS_DIR, 'charades_st
 CHARADES_ANNOTATIONS_TEST = os.path.join(CHARADES_ANNOTATIONS_DIR, 'charades_sta_test.txt')
 
 # Save model directory
-SAVE_MODEL_DIR = os.path.join(BASE_DIR, 'saved_models')
+SAVE_MODEL_DIR = os.path.join(BASE_DIR, 'generated_models')
 COARSE_GRAINED_MODELS_DIR = os.path.join(SAVE_MODEL_DIR, 'coarse_grained')
 FINE_GRAINED_MODELS_DIR = os.path.join(SAVE_MODEL_DIR, 'fine_grained')
