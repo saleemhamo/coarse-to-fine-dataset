852c5694a8a52f844d2ebe71a31c7d360350460c
saleemhamo <saleemhamo@gmail.com>
2024-08-13 12:51:58 +0300

    Replaced directories with submodules for T-MASS, T-MASS-V2, and MESM

--- data/__init__.py
+++
@@ -1,4 +0,0 @@
-from .datasets import get_dataset
-from .charades_sta import CharadesSTA
-
-__all__ = ['get_dataset', 'CharadesSTA']
--- utils/__init__.py
+++
@@ -1,4 +0,0 @@
-from .constants import *
-from .model_utils import *
-
-__all__ = ['constants', 'model_utils', 'save_model', 'get_device']
