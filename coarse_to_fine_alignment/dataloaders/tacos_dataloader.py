0db75d3f3d54a84ab083010f4190eca7d30a47cc
saleemhamo <saleemhamo@gmail.com>
2024-08-17 14:46:23 +0300

    coarse_to_fine_alignment dataloader

--- coarse_to_fine_alignment/dataloaders/tacos_dataloader.py
+++ coarse_to_fine_alignment/dataloaders/tacos_dataloader.py
@@ -9,7 +9,7 @@ class TACoSDataset(Dataset):
         self.coarse_summaries = self.load_coarse_summaries(coarse_summaries_path)
         self.tokenizer = tokenizer
         self.max_len = max_len
-        self.video_ids = list(self.fine_annotations.keys())
+        self.video_ids = list(self.fine_annotations.keys())  # List of video IDs
 
     def load_fine_annotations(self, file_path):
         with open(file_path, 'r') as f:
@@ -28,35 +28,30 @@ class TACoSDataset(Dataset):
         return len(self.video_ids)
 
     def __getitem__(self, idx):
-        fine_text = self.fine_annotations[idx]
-        coarse_text = self.coarse_summaries[idx]
+        video_id = self.video_ids[idx]  # Use the video ID from the list
+        fine_text = " ".join(self.fine_annotations[video_id]['sentences'])  # Combine sentences if needed
+        coarse_text = self.coarse_summaries[video_id]  # Get the corresponding coarse summary
+
         inputs = self.tokenizer.encode_plus(
             fine_text,
             add_special_tokens=True,
             max_length=self.max_len,
-            pad_to_max_length=True,
+            padding="max_length",
             return_attention_mask=True,
             return_tensors="pt"
         )
+
         labels = self.tokenizer.encode_plus(
             coarse_text,
             add_special_tokens=True,
             max_length=self.max_len,
-            pad_to_max_length=True,
+            padding="max_length",
             return_attention_mask=False,
             return_tensors="pt"
         )
 
-        # Use only the first token of the label (which represents the whole sequence in sequence classification)
         return {
             'input_ids': inputs['input_ids'].flatten(),
             'attention_mask': inputs['attention_mask'].flatten(),
             'labels': labels['input_ids'].flatten()[0],  # Take the first token as the label
         }
-
-
-# Utility function to load data paths
-def load_data_paths():
-    fine_file_path = os.path.join("../data", 'tacos', "tacos.json")
-    coarse_file_path = os.path.join("../data", 'tacos', "tacos_cg.json")
-    return fine_file_path, coarse_file_path
