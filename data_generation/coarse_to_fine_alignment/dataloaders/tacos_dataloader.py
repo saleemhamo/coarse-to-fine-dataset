6ab12ca759af8e86adf57e64c38b2bb3f38ae7a9
saleemhamo <saleemhamo@gmail.com>
2024-08-18 15:20:49 +0300

    workaround config file

--- data_generation/coarse_to_fine_alignment/dataloaders/tacos_dataloader.py
+++ data_generation/coarse_to_fine_alignment/dataloaders/tacos_dataloader.py
@@ -2,9 +2,7 @@ import json
 import torch
 import logging
 from torch.utils.data import Dataset
-from collections import defaultdict
 
-# Configure logging
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
 
@@ -23,10 +21,13 @@ class TACoSDataset(Dataset):
     def load_coarse_summaries(self, file_path):
         with open(file_path, 'r') as f:
             data = json.load(f)
-            summaries = defaultdict(list)
+            summaries = {}
             for item in data:
                 video_id = item['video_id']
-                summaries[video_id].append(item['summarized_sentence'])
+                if video_id in summaries:
+                    summaries[video_id].append(item['summarized_sentence'])
+                else:
+                    summaries[video_id] = [item['summarized_sentence']]
             return summaries
 
     def __len__(self):
@@ -35,10 +36,7 @@ class TACoSDataset(Dataset):
     def __getitem__(self, idx):
         video_id = self.video_ids[idx]
         fine_text = self.fine_annotations[video_id]['sentences']
-
-        # Get all coarse summaries for the given video ID
         coarse_texts = self.coarse_summaries.get(video_id, ["[UNK]"])
-        coarse_text = ' '.join(coarse_texts)  # Combine all coarse summaries into one text
 
         inputs = self.tokenizer.encode_plus(
             fine_text,
@@ -49,23 +47,18 @@ class TACoSDataset(Dataset):
             return_tensors="pt"
         )
 
-        labels = self.tokenizer.encode_plus(
+        labels = [self.tokenizer.encode(
             coarse_text,
             add_special_tokens=True,
             max_length=self.max_len,
             padding='max_length',
-            return_attention_mask=False,
             return_tensors="pt"
-        )
-
-        if labels['input_ids'].numel() == 0:
-            logging.error(f"Empty label tensor for video_id {video_id}, coarse_text: '{coarse_text}'")
-            labels['input_ids'] = torch.tensor([self.tokenizer.cls_token_id] * self.max_len)
+        ).squeeze(0) for coarse_text in coarse_texts]
 
         return {
             'input_ids': inputs['input_ids'].flatten(),
             'attention_mask': inputs['attention_mask'].flatten(),
-            'labels': labels['input_ids'].flatten()
+            'labels': torch.stack(labels)
         }
 
 
@@ -76,17 +69,9 @@ def collate_fn(batch):
     attention_mask = [item['attention_mask'] for item in batch]
     labels = [item['labels'] for item in batch]
 
-    valid_labels = []
-    for label in labels:
-        if label.dim() != 0:
-            valid_labels.append(label)
-
-    if not valid_labels:
-        raise ValueError("All labels are invalid.")
-
     input_ids = torch.nn.utils.rnn.pad_sequence(input_ids, batch_first=True, padding_value=0)
     attention_mask = torch.nn.utils.rnn.pad_sequence(attention_mask, batch_first=True, padding_value=0)
-    labels = torch.nn.utils.rnn.pad_sequence(valid_labels, batch_first=True, padding_value=0)
+    labels = torch.nn.utils.rnn.pad_sequence(labels, batch_first=True, padding_value=0)
 
     return {
         'input_ids': input_ids,
