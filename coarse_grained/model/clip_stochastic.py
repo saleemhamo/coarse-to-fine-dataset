3a04d93b4501fc318129718755d89330bace91e4
saleemhamo <saleemhamo@gmail.com>
2024-08-22 10:51:48 +0300

    test clip

--- coarse_grained/model/clip_stochastic.py
+++ coarse_grained/model/clip_stochastic.py
@@ -27,37 +27,35 @@ class CLIPStochastic(nn.Module):
         video_data = data['video']
         video_data = video_data.reshape(-1, 3, self.config.input_res, self.config.input_res)
 
-        try:
-            if is_train:
-                text_features = self.clip.get_text_features(**text_data)
-                video_features = self.clip.get_image_features(video_data)
+        if is_train:
 
-                # print(f"[TRAIN] Video features shape before reshape: {video_features.shape}")
-                video_features = video_features.reshape(batch_size, self.config.num_frames, -1)
-                video_features_pooled = self.pool_frames(text_features, video_features)
+            text_features = self.clip.get_text_features(**text_data)
+            video_features = self.clip.get_image_features(video_data)
 
-                text_features_stochstic, text_mean, log_var = self.stochastic(text_features, video_features)
+            video_features = video_features.reshape(batch_size, self.config.num_frames, -1)  # [bs, #F, 512]
 
-                if return_all_frames:
-                    return text_features, video_features, video_features_pooled, text_features_stochstic, text_mean, log_var
+            video_features_pooled = self.pool_frames(text_features, video_features)
 
-                return text_features, video_features_pooled, text_features_stochstic, text_mean, log_var
+            # @WJM: perform stochastic text
+            text_features_stochstic, text_mean, log_var = self.stochastic(text_features, video_features)
 
-            else:
-                text_features = self.clip.get_text_features(**text_data)
-                video_features = self.clip.get_image_features(video_data)
+            if return_all_frames:
+                return text_features, video_features, video_features_pooled, text_features_stochstic, text_mean, log_var
 
-                # print(f"[EVAL] Video features shape before reshape: {video_features.shape}")
-                video_features = video_features.reshape(batch_size, self.config.num_frames, -1)
-                video_features_pooled = self.pool_frames(text_features, video_features)
+            return text_features, video_features_pooled, text_features_stochstic, text_mean, log_var
 
-                text_features_stochstic, _, _ = self.stochastic(text_features, video_features)
+        else:
 
-                if return_all_frames:
-                    return text_features, video_features, video_features_pooled, text_features_stochstic
+            text_features = self.clip.get_text_features(**text_data)
+            video_features = self.clip.get_image_features(video_data)
 
-                return text_features, video_features_pooled, text_features_stochstic
+            video_features = video_features.reshape(batch_size, self.config.num_frames, -1)
+            video_features_pooled = self.pool_frames(text_features, video_features)
 
-        except Exception as e:
-            print(f"[ERROR] Exception occurred in forward method: {e}")
-            return None
+            # @WJM: re-parameterization for text (independent of the text-cond pooling)
+            text_features_stochstic, _, _ = self.stochastic(text_features, video_features)
+
+            if return_all_frames:
+                return text_features, video_features, video_features_pooled, text_features_stochstic
+
+            return text_features, video_features_pooled, text_features_stochstic
